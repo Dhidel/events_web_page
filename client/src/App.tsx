3b22433a1fdf2f79fab18854177f0faa593918b2
Alexander L. <alexavion1028@gmail.com>
2026-09-08 07:57:31 -0600

    scroll top delete

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
-import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import Servicios from "./pages/Servicios";
 import Cotizador from "./pages/Cotizador";
